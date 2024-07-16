7f3526dc23e23dc0162fd9c5f20a9db90134e003
Tan Nguyen <tannm132@outlook.com>
2024-07-16 18:41:38 +0700

    Cleanup unused files

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # class ApplicationMailer
-class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
-end
+# class ApplicationMailer < ActionMailer::Base
+#   default from: 'from@example.com'
+#   layout 'mailer'
+# end
